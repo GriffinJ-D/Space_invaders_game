7b8c4873f757832cf09aff48179d59868f40dfef
GriffinJ-D <65211446+GriffinJ-D@users.noreply.github.com>
2020-06-21 15:57:17 -0700

    Update hope.py

--- hope.py
+++ hope.py
@@ -19,16 +19,20 @@ icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
 yours = spaceship(pygame.image.load('spaceship!.png'), 380, 516, 0, 0, 3, 3, 0, 0)
-Enemy = spaceship(pygame.image.load('enemy.png'), random.randint(0, 735), random.randint(50, 150), 0.4, 0, 1, 1, 0, 0)
+Boss = spaceship(pygame.image.load('kraken.png'), 380, 65, 0, 0, 5, 5, 0, 0)
+Enemy = spaceship(pygame.image.load('enemy.png'), random.randint(0, 735), random.randint(100, 160), 0.4, 0, 1, 1, 0, 0)
 # Enemy3 = spaceship(pygame.image.load('enemy.png'), random.randint(0, 735), random.randint(50, 150), 0.5, 0, 1, 1)
-Enemy1 = spaceship(pygame.image.load('ufo (1).png'), random.randint(0, 735), random.randint(50, 150), 0.1, 0, 3, 3, 0, 0)
-Enemy2 = spaceship(pygame.image.load('ufo (2).png'), random.randint(0, 735), random.randint(50, 150), 0.25, 0, 2, 2, 0, 0)
+Enemy1 = spaceship(pygame.image.load('ufo (1).png'), random.randint(0, 735), random.randint(100, 160), 0.1, 0, 3, 3, 0, 0)
+Enemy2 = spaceship(pygame.image.load('ufo (2).png'), random.randint(0, 735), random.randint(100, 160), 0.25, 0, 2, 2, 0, 0)
 Barrier1 = barrier(pygame.image.load('mansory.png'), 380, 400, 5, 5)
 Barrier2 = barrier(pygame.image.load('mansory.png'), 170, 400, 5, 5)
 Barrier3 = barrier(pygame.image.load('mansory.png'), 590, 400, 5, 5)
 BarrierX = [Barrier1.X, Barrier2.X, Barrier3.X]
 BarrierY = [Barrier1.Y, Barrier2.Y, Barrier3.Y]
 BarrierHealth = [Barrier1.CHP, Barrier2.CHP, Barrier3.CHP]
+
+shield = pygame.Rect(600, 430, 200, 40)
+laser = pygame.Rect(Boss.X - 13000, Boss.Y, 26, 800)
 
 EnemyX = [Enemy.X, Enemy1.X, Enemy2.X]
 EnemyY = [Enemy.Y, Enemy1.Y, Enemy2.Y]
@@ -38,12 +42,14 @@ EnemyYchange = [Enemy.speedY, Enemy1.speedY, Enemy2.speedY]
 EnemyHP = [Enemy.CHP, Enemy1.CHP, Enemy2.CHP]
 Enemy_health = [Enemy.health, Enemy1.health, Enemy2.health]
 
-yourbullet = bullet(pygame.image.load('bullet.png'), 0, 0, 0.7, 0, 0, True, 0, 0)
+yourbullet = bullet(pygame.image.load('bullet.png'), 0, 0, 1, 0, 0, True, 0, 0)
 enemybullet = bullet(pygame.image.load('bulletrotated.png'), 0, 0, 0.7, 0, 0, True, 0, 0)
-current_bullet_angle = []
+# shieldbullet = bullet()
 
 score_value = 0
 font = pygame.font.Font('freesansbold.ttf', 32)
+color = (255, 0, 0)
+before = time.time()
 
 TextX = 10
 TextY = 10
@@ -53,11 +59,24 @@ def do_math(angle, speed):
     yourbullet.Y_change = math.cos(your_radians) * speed
     yourbullet.X_change = math.sin(your_radians) * speed
 
+win_font = pygame.font.Font('freesansbold.ttf', 64)
+def game_won():
+    win_text = win_font.render("YOU WIN", True, (255, 255, 255))
+    screen.blit(win_text, (200, 250))
+    mixer.music.stop()
+    Boss.X -= 9000
+    for j in range(len(EnemyX)):
+        EnemyY[j] = -200000
+    for j in range(len(EnemyX)):
+        EnemyX[j] = -200000
 
 over_font = pygame.font.Font('freesansbold.ttf', 64)
-def game_over_text():
-    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+def game_over_text(either):
+    over_text = over_font.render(either, True, (255, 255, 255))
     screen.blit(over_text, (200, 250))
+    mixer.music.stop()
+    for j in range(len(EnemyX)):
+        EnemyX[j] = -200000
 
 def enemy_shoot(ship):
     bullet_sound = mixer.Sound('laser.wav')
@@ -75,10 +94,13 @@ def barrier(x, y):
     # screen.blit(Barrier1.image, (x, y))
     screen.blit(Barrier1.image, (x - int(Barrier1.image.get_width() / 2), y - int(Barrier1.image.get_height() / 2)))
 
-def player(x, y,):
+def player(x, y):
     img_copy = pygame.transform.rotate(yours.image, yours.angle)
     screen.blit(img_copy, (x - int(img_copy.get_width() / 2), y - int(img_copy.get_height() / 2)))
     # screen.blit(pygame.transform.rotate(yours.image, yours.angle), (x, y))
+
+def boss(x, y):
+    screen.blit(Boss.image, (x - int(Boss.image.get_width() / 2), y - int(Boss.image.get_height() / 2)))
 
 
 def enemy(x, y, i):
@@ -107,6 +129,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_f:
+                # shield = pygame.Rect(yours.X - 100, 430, 200, 40)
+                pygame.draw.rect(screen, pygame.Color('grey12'), shield)
             if event.key == pygame.K_a:
                 yours.rotation += 0.3
                 yourbullet.rotation += yours.rotation
@@ -114,6 +139,10 @@ while running:
                 # yourbullet.rotation += -0.3
                 yourbullet.rotation += yours.rotation
                 yours.rotation += -0.3
+            if event.key == pygame.K_UP:
+                yours.speedY = 0.2
+            if event.key == pygame.K_DOWN:
+                yours.speedY = -0.2
             if event.key == pygame.K_LEFT:
                 yours.speedX = -0.2
             if event.key == pygame.K_RIGHT:
@@ -134,14 +163,60 @@ while running:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 yours.speedX = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                yours.speedY = 0
             if event.key == pygame.K_a or event.key == pygame.K_d:
                 yours.rotation = 0
     yours.angle += yours.rotation
-    if yours.angle >= 70:
-        yours.angle = 70
-    elif yours.angle <= -70:
-        yours.angle = -70
+    # if yours.angle >= 70:
+    #     yours.angle = 70
+    # elif yours.angle <= -70:
+    #     yours.angle = -70
 
+    # laser = pygame.Rect(Boss.X - 13000, Boss.Y, 26, 800)
+    after = time.time()
+    # LASER SOUND (NOT WORKING)
+    # while laser.centerx > 0:
+    #     laser_sound = mixer.Sound('lasersoundtest.wav')
+    #     laser_sound.play()
+
+    laser_yours_ = isCollision(yours.X, yours.Y, Boss.X, yours.Y, 50)
+    if before + 2 < after and not laser_yours_:
+        laser = pygame.Rect(Boss.X - 13, Boss.Y, 26, 800)
+        pygame.draw.rect(screen, color, laser)
+
+        # before = time.time()
+    laser_yours = isCollision(yours.X, yours.Y, laser.centerx, yours.Y, 50)
+    if before + 2 < after and laser_yours:
+        if laser_yours_:
+            hit_sound = mixer.Sound('explosion.wav')
+            hit_sound.play()
+            yours.CHP -= 1
+            print(yours.CHP)
+            laser = pygame.Rect(-200, Boss.Y, 26, 800)
+            before = time.time()
+            if yours.CHP <= 0:
+                game_over_text("GAME OVER")
+
+    boss_hit = isCollision(Boss.X, Boss.Y, yourbullet.X, yourbullet.Y, 75)
+    if boss_hit:
+        hit_sound = mixer.Sound('explosion.wav')
+        hit_sound.play()
+        yourbullet.Y = 800
+        yourbullet.ready = True
+        Boss.CHP -= 1
+        print(Boss.CHP)
+        if Boss.CHP <= 0:
+            game_won()
+
+
+    if Boss.X < yours.X:
+        Boss.X += 0.1
+    if Boss.X > yours.X:
+        Boss.X -= 0.1
+
+
+    yours.Y -= yours.speedY
     yours.X += yours.speedX
     if yours.X < -50:
         yours.X = 790 
@@ -166,12 +241,9 @@ while running:
             for j in range(len(EnemyY)):
                 EnemyY[j] = 10000
         if EnemyY[i] > 800:
-            over_text1 = over_font.render("GAME OVER", True, (255, 255, 255))
-            screen.blit(over_text1, (200, 250))
-            mixer.music.stop()
-            for j in range(len(EnemyX)):
-                EnemyX[j] = -200000
-            break
+            game_over_text("GAME OVER")
+        if EnemyY[i] < -800:
+            game_over_text("GAME WON!")
 
 
         they_hit = isCollision(EnemyX[i], EnemyY[i], yourbullet.X, yourbullet.Y, 35)
@@ -192,7 +264,7 @@ while running:
     if enemybullet.ready:
         enemy_shoot(numb)
 
-    if not isCollision(yourbullet.X, yourbullet.Y, yours.X, yours.Y, 1000):
+    if not isCollision(yourbullet.X, yourbullet.Y, yours.X, yours.Y, 800):
         yourbullet.ready = True
     if not yourbullet.ready:
         fire_bullet(yourbullet, yourbullet.X, yourbullet.Y, yourbullet.angle)
@@ -214,12 +286,29 @@ while running:
         enemybullet.Y = 600
         print(yours.CHP)
         if yours.CHP <= 0:
-            game_over_text()
-            for j in range(len(EnemyX)):
-                EnemyX[j] = -200000
+            game_over_text("GAME OVER")
+
 
     for i in range(len(BarrierX)):
         barrier(BarrierX[i], BarrierY[i])
+        # after = time.time()
+        # laser_barrier_ = isCollision(BarrierX[i], BarrierY[i], Boss.X, BarrierY[i], 50)
+        # if before + 2 < after and not laser_barrier_:
+        #     laser = pygame.Rect(Boss.X - 13, Boss.Y, 26, 800)
+        #     pygame.draw.rect(screen, color, laser)
+        #
+        #     # before = time.time()
+        # laser_barrier = isCollision(BarrierX[i], BarrierY[i], laser.centerx, BarrierY[i], 50)
+        # if before + 2 < after and laser_barrier:
+        #     if laser_yours_:
+        #         hit_sound = mixer.Sound('explosion.wav')
+        #         hit_sound.play()
+        #         laser = pygame.Rect(-200, Boss.Y, 26, 800)
+        #         before = time.time()
+        #         BarrierHealth[i] -= 1
+        #         if BarrierHealth[i] <= 0:
+        #             BarrierX[i] = 2000
+        #             BarrierY[i] = 50
         barrier_hit = isCollision(BarrierX[i], BarrierY[i], enemybullet.X, enemybullet.Y, 37)
         if barrier_hit:
             hit_sound = mixer.Sound('explosion.wav')
@@ -239,7 +328,12 @@ while running:
             if BarrierHealth[i] <= 0:
                 BarrierX[i] = 2000
                 BarrierY[i] = 50
+        stop = isCollision(yours.X, yours.Y, BarrierX[i], BarrierY[i], 65)
+        if stop:
+            yours.X -= yours.speedX
+            yours.Y += yours.speedY
+
+    boss(Boss.X, Boss.Y)
     player(yours.X, yours.Y)
     show_score(TextX, TextY)
     pygame.display.update()
-
